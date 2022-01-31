8bb8ca236411989e27be634d1b46c703573f5799
ProstoVitya <sovsemnevitya@gmail.com>
2022-01-31 10:01:27 +0400

    Player Controller
    First attampt in player controller scripting using rigid body physics

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField] private float _xSensitivity;
+    [SerializeField] private float _ySensitivity;
+
+    private Camera _playerCamera;
+    private float _mouseX;
+    private float _mouseY;
+
+    private float _multiplier = 0.01f;
+
+    private float _xRotation;
+    private float _yRotation;
+
+    private void Start()
+    {
+        _playerCamera = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        _playerCamera.transform.localRotation = Quaternion.Euler(_yRotation, 0, 0);
+        transform.localRotation = Quaternion.Euler(0, _xRotation, 0);
+    }
+
+    private void HandleInput()
+    {
+        _mouseX = Input.GetAxisRaw("Mouse X");
+        _mouseY = Input.GetAxisRaw("Mouse Y");
+
+        _xRotation += _mouseX * _xSensitivity * _multiplier;
+        _yRotation -= _mouseY * _ySensitivity * _multiplier;
+
+        _yRotation = Mathf.Clamp(_yRotation, -90f, 90f);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    private const float GroundDrag = 6f;
+    private const float AirDrag = 2f;
+    private const float PlayerHeight = 2f;
+
+    [Header("Movement")]
+    public float MovementSpeed;
+    [SerializeField] private float _airMultiplier = 0.4f;
+    private const float SpeedMultiplier = 10f;
+
+    [Header("Jumping")]
+    public float JumpForce = 5f;
+
+    [Header("Keybinds")]
+    [SerializeField] private KeyCode _jumpKey = KeyCode.Space;
+
+    private float _verticalMovement;
+    private float _horizontalMovement;
+    private Vector3 _moveDirection;
+    private Rigidbody _rigidBody;
+    private bool _isGrounded;
+
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _rigidBody.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        _isGrounded = Physics.Raycast(transform.position, Vector3.down, PlayerHeight / 2f + 0.1f);
+
+        HandleInput();
+        ControlDrag();
+
+        if (Input.GetKeyDown(_jumpKey) && _isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        _rigidBody.AddForce(transform.up * JumpForce, ForceMode.Impulse);
+    }
+
+    private void HandleInput()
+    {
+        _horizontalMovement = Input.GetAxisRaw("Horizontal");
+        _verticalMovement = Input.GetAxisRaw("Vertical");
+
+        _moveDirection = transform.forward * _verticalMovement + transform.right * _horizontalMovement;
+    }
+
+    private void ControlDrag()
+    {
+        _rigidBody.drag = _isGrounded ? GroundDrag : AirDrag;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();    
+    }
+
+    private void MovePlayer()
+    {
+        if(_isGrounded)
+            _rigidBody.AddForce(_moveDirection.normalized * MovementSpeed * SpeedMultiplier,
+                ForceMode.Acceleration);
+        else
+            _rigidBody.AddForce(_moveDirection.normalized * MovementSpeed * SpeedMultiplier *
+                _airMultiplier, ForceMode.Acceleration);
+    }
+}
